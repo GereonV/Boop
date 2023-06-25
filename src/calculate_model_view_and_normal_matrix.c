7c96315f31857f03e55fbb230f60ef33ca746216
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-06-25 16:03:51 +0200

    Fix matrix calculations

--- src/calculate_model_view_and_normal_matrix.c
+++ src/calculate_model_view_and_normal_matrix.c
@@ -6,9 +6,9 @@
 void calculate_model_view_and_normal_matrix(mat4 * restrict mv, mat3 * restrict n, object_t const * restrict o, vec3 cp) {
 	float s = sinf(o->z_rotation_radians);
 	float c = cosf(o->z_rotation_radians);
-	AT(n, 0, 0) = s; AT(n, 1, 0) = -c;
-	AT(n, 0, 1) = c; AT(n, 1, 1) =  s;
-	AT(mv, 0, 0) = o->scale.x * s; AT(mv, 1, 0) = -o->scale.y * c; AT(mv, 3, 0) = o->position.x - cp.x;
-	AT(mv, 0, 1) = o->scale.y * c; AT(mv, 1, 1) =  o->scale.y * s; AT(mv, 3, 1) = o->position.y - cp.y;
+	AT(n, 0, 0) = c; AT(n, 1, 0) = -s;
+	AT(n, 0, 1) = s; AT(n, 1, 1) =  c;
+	AT(mv, 0, 0) = o->scale.x * c; AT(mv, 1, 0) = -o->scale.y * s; AT(mv, 3, 0) = o->position.x - cp.x;
+	AT(mv, 0, 1) = o->scale.y * s; AT(mv, 1, 1) =  o->scale.y * c; AT(mv, 3, 1) = o->position.y - cp.y;
 	AT(mv, 2, 2) = o->scale.z;                                     AT(mv, 3, 2) = o->position.z - cp.z;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -23,25 +23,6 @@ typedef struct {
 
 // model_view and normal are assumed to be identities or previous results
 void calculate_model_view_and_normal_matrix(mat4 * model_view, mat3 * normal, object_t const * object, vec3 camera_position);
-
-// x -> -near*x/z
-// y -> -near*y/z
-// z -> -2/(far-near)*(near+z)-1
-//
-// [-near     0  0  0][x]   [-near*x]
-// [    0 -near  0  0][y] - [-near*y]
-// [    0     0 m1 m2][z] - [m1*z+m2]
-// [    0     0  1  0][1]   [z]
-//
-// m1*z+m2=z*(-2/(far-near)*(near+z)-1)
-// z has two solutions; we pick -near and -far
-// m1*-near+m2=near ∧ m1*-far+m2=-far
-// m1=(far+near)/(far-near) ∧ m2=2*far*near/(far-near)
-#define NEAR 0.1f
-#define FAR  5.0f
-constexpr mat4 perspective_projection_matrix = {
-	{-NEAR}, {[1] = -NEAR}, {[2] = (FAR + NEAR)/(FAR - NEAR), 1.0f}, {[2] = 2 * FAR * NEAR / (FAR - NEAR)},
-};
-// TODO FOV
+mat4 const * calculate_projection_matrix(float fov);
 
 #endif
